782f1c250a803212a0a0a40ce8348d2cbe103bc9
Sheena Bonilla <sheena.myb@gmail.com>
2022-07-25 10:50:29 -0700

    commit tree b0bb2f19fb1262529aec2f2d41f0bd73d78f84bc

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import React, {Component} from 'react';
+
+export default class App extends Component {
+
+    state = {
+        peopleSpace: []
+    }
+
+    componentDidMount() {
+        fetch(`http://api.open-notify.org/astros.json`)
+        .then(resp => resp.json())
+        .then(({people}) => this.setState({peopleSpace: people}))
+    }
+    
+    render() {
+        return (<div>
+            {this.state.peopleSpace.map((person, id) => <h1 key={id}>{person.name}</h1>)}
+        </div>)
+    }
+}
